624ae55fd0341de25195f677c75db5566fbd7ad2
Stephen Cavaliere <stephencavaliere@gmail.com>
2014-09-27 18:28:11 -0400

    Added mocha testing

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,18 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('lint', function eslint() {
-  gulp.src([ 'app/**/*.js', 'test/**/*.js', 'gulpfile.js', 'server.js' ])
+  return gulp.src([ 'app/**/*.js', 'test/**/*.js', 'gulpfile.js', 'server.js' ])
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format());
+});
+
+gulp.task('test', function mocha() {
+  return gulp.src([ 'test/**/*.test.js' ])
+    .pipe(plugins.mocha({
+      ui: 'bdd',
+      reporter: 'spec',
+      growl: true
+    }));
 });
 
 gulp.task('default', [ 'lint' ]);
